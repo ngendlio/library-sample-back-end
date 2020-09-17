a042368f79327e65f5f6a5efd25efa4ce0f55310
Lionel Ngendakuriyo <ngendlio@gmail.com>
2020-09-17 09:22:04 +0200

    First commit ever bro.

---
+++ src/main/kotlin/com/example/libraryapp/LibraryappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.libraryapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryappApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryappApplication>(*args)
+}
